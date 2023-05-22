162baf5b0c6bb270514102d4bdb940659bd695e5
hsnrzky <hasanrizky0405@gmail.com>
2023-05-23 05:35:12 +0800

    Coding Project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+var header = document.getElementById("header");
 var outputLuas = document.getElementById("output_luas");
 var outputKeliling = document.getElementById("output_keliling");
 var persegi = document.getElementById("persegi");
@@ -7,18 +8,23 @@ var msg = document.getElementById("msg");
 msg.style.display = "none"
 main.style.display = "none"
 
+// untuk merumuskan Luas dan memanggil output
 function luas() {
     var sisi_luas = document.getElementById("sisi-luas").value;
     var luas = sisi_luas * sisi_luas;
     outputLuas.style.display = "block";
     outputLuas.innerHTML = "L = S x S<br>L = " + sisi_luas + " x " + sisi_luas + "<br>L = " + luas;
 }
+
+// untuk merumusan Keliling dan memanggil output
 function keliling() {
     var sisi_keliling = document.getElementById("sisi-keliling").value;
     var keliling = 4 * sisi_keliling;
     outputKeliling.style.display = "block";
     outputKeliling.innerHTML = "K = 4 x S<br>K = 4 x " + sisi_keliling + "<br>K = " + keliling;
 }
+
+// untuk mereset dan menghapsu output
 function reset() {
     var resetLuas = document.getElementById("reset-luas");
     var resetKeliling = document.getElementById("reset-keliling");
@@ -30,6 +36,8 @@ function reset() {
     }
     
 }
+
+// tombol untuk mengganti pilihan
 function square() {
     if (persegi){
         persegi.style.backgroundColor = "rgb(39, 187, 245)";
@@ -50,9 +58,16 @@ function rectangle() {
         msg.style.display = "block";
     }
 }
+
+// mengadakan event untuk memanggil fungsi
 document.getElementById("hitung-luas").addEventListener("click", luas);
 document.getElementById("hitung-keliling").addEventListener("click", keliling);
 document.getElementById("reset-luas").addEventListener("click", reset);
 document.getElementById("reset-keliling").addEventListener("click", reset);
 persegi.addEventListener("click", square);
 persegiPanjang.addEventListener("click", rectangle);
+
+// fungsi untuk reload
+header.addEventListener("click", function() {
+    window.location.reload();
+});
